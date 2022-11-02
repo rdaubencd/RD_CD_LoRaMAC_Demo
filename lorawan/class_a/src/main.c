b68182605b8f7c9049c9d390b85e24e8788d0af8
Ron Daubenspeck <ron.daubenspeck@connecteddev.com>
2022-11-02 14:16:41 -0400

    Added the lorawan sample app and modified it to run on nRF52840DK.

--- lorawan/class_a/src/main.c
+++ lorawan/class_a/src/main.c
@@ -6,110 +6,135 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/device.h>
-#include <zephyr/lorawan/lorawan.h>
-#include <zephyr/zephyr.h>
+#include <device.h>
+#include <lorawan/lorawan.h>
+#include <zephyr.h>
 
 /* Customize based on network configuration */
-#define LORAWAN_DEV_EUI			{ 0xDD, 0xEE, 0xAA, 0xDD, 0xBB, 0xEE,\
-					  0xEE, 0xFF }
-#define LORAWAN_JOIN_EUI		{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,\
-					  0x00, 0x00 }
-#define LORAWAN_APP_KEY			{ 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE,\
-					  0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88,\
-					  0x09, 0xCF, 0x4F, 0x3C }
+#define LORAWAN_DEV_EUI       { 0x00, 0x80, 0x00, 0x00, 0x04, 0x01, 0xdd, 0x40 }
+#define LORAWAN_JOIN_EUI      { 0xa2, 0xb3, 0x84, 0x25, 0xcf, 0xb6, 0xf7, 0xfe }
+#define LORAWAN_APP_KEY       { 0x87, 0x47, 0xcc, 0xc8, 0xce, 0x01, 0xd2, 0x96, 0x2d, 0x5f, 0x94, 0x60, 0x0b, 0xcd, 0x38, 0xcf }
 
-#define DELAY K_MSEC(10000)
+#define DELAY K_MSEC(4000)
 
 #define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
-#include <zephyr/logging/log.h>
+#include <logging/log.h>
 LOG_MODULE_REGISTER(lorawan_class_a);
 
-char data[] = {'h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd'};
+char data[] = {'h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd', '0', '0', '0', '0'};
 
 static void dl_callback(uint8_t port, bool data_pending,
-			int16_t rssi, int8_t snr,
-			uint8_t len, const uint8_t *data)
+                        int16_t rssi, int8_t snr,
+                        uint8_t len, const uint8_t *data)
 {
-	LOG_INF("Port %d, Pending %d, RSSI %ddB, SNR %ddBm", port, data_pending, rssi, snr);
-	if (data) {
-		LOG_HEXDUMP_INF(data, len, "Payload: ");
-	}
+   LOG_INF("RX Port %d, Pending %d, RSSI %ddB, SNR %ddBm", port, data_pending, rssi, snr);
+   if (data)
+   {
+      LOG_HEXDUMP_INF(data, len, "RX Payload: ");
+   }
 }
 
 static void lorwan_datarate_changed(enum lorawan_datarate dr)
 {
-	uint8_t unused, max_size;
+   uint8_t unused, max_size;
 
-	lorawan_get_payload_sizes(&unused, &max_size);
-	LOG_INF("New Datarate: DR_%d, Max Payload %d", dr, max_size);
+   lorawan_get_payload_sizes(&unused, &max_size);
+   LOG_INF("New Datarate: DR_%d, Max Payload %d", dr, max_size);
 }
 
 void main(void)
 {
-	const struct device *lora_dev;
-	struct lorawan_join_config join_cfg;
-	uint8_t dev_eui[] = LORAWAN_DEV_EUI;
-	uint8_t join_eui[] = LORAWAN_JOIN_EUI;
-	uint8_t app_key[] = LORAWAN_APP_KEY;
-	int ret;
+   const struct device *lora_dev;
+   struct lorawan_join_config join_cfg;
+   uint8_t dev_eui[] = LORAWAN_DEV_EUI;
+   uint8_t join_eui[] = LORAWAN_JOIN_EUI;
+   uint8_t app_key[] = LORAWAN_APP_KEY;
+   int ret;
+   uint32_t counter = 0;
 
-	struct lorawan_downlink_cb downlink_cb = {
-		.port = LW_RECV_PORT_ANY,
-		.cb = dl_callback
-	};
+   struct lorawan_downlink_cb downlink_cb =
+   {
+      .port = LW_RECV_PORT_ANY,
+      .cb = dl_callback
+   };
 
-	lora_dev = DEVICE_DT_GET(DT_ALIAS(lora0));
-	if (!device_is_ready(lora_dev)) {
-		LOG_ERR("%s: device not ready.", lora_dev->name);
-		return;
-	}
+   LOG_INF("Version 1.0  Build: %s %s", __DATE__, __TIME__);
 
-	ret = lorawan_start();
-	if (ret < 0) {
-		LOG_ERR("lorawan_start failed: %d", ret);
-		return;
-	}
+   lora_dev = DEVICE_DT_GET(DT_ALIAS(lora0));
+   if (!device_is_ready(lora_dev))
+   {
+      LOG_ERR("%s: device not ready.", lora_dev->name);
+      return;
+   }
 
-	lorawan_register_downlink_callback(&downlink_cb);
-	lorawan_register_dr_changed_callback(lorwan_datarate_changed);
+   ret = lorawan_start();
+   if (ret < 0)
+   {
+      LOG_ERR("lorawan_start failed: %d", ret);
+      return;
+   }
 
-	join_cfg.mode = LORAWAN_ACT_OTAA;
-	join_cfg.dev_eui = dev_eui;
-	join_cfg.otaa.join_eui = join_eui;
-	join_cfg.otaa.app_key = app_key;
-	join_cfg.otaa.nwk_key = app_key;
+   lorawan_register_downlink_callback(&downlink_cb);
+   lorawan_register_dr_changed_callback(lorwan_datarate_changed);
 
-	LOG_INF("Joining network over OTAA");
-	ret = lorawan_join(&join_cfg);
-	if (ret < 0) {
-		LOG_ERR("lorawan_join_network failed: %d", ret);
-		return;
-	}
+   join_cfg.mode = LORAWAN_ACT_OTAA;
+   join_cfg.dev_eui = dev_eui;
+   join_cfg.otaa.join_eui = join_eui;
+   join_cfg.otaa.app_key = app_key;
+   join_cfg.otaa.nwk_key = app_key;
 
-	LOG_INF("Sending data...");
-	while (1) {
-		ret = lorawan_send(2, data, sizeof(data),
-				   LORAWAN_MSG_CONFIRMED);
+   for (int i = 0; i < 16; i++)
+   {
+      LOG_INF("Joining network over OTAA. Attempt #%d", i);
+      ret = lorawan_join(&join_cfg);
+      if (ret < 0)
+      {
+         LOG_ERR("lorawan_join_network failed: %d", ret);
 
-		/*
-		 * Note: The stack may return -EAGAIN if the provided data
-		 * length exceeds the maximum possible one for the region and
-		 * datarate. But since we are just sending the same data here,
-		 * we'll just continue.
-		 */
-		if (ret == -EAGAIN) {
-			LOG_ERR("lorawan_send failed: %d. Continuing...", ret);
-			k_sleep(DELAY);
-			continue;
-		}
+         if ((i % 2) == 1)
+         {
+            k_sleep(K_MSEC(6000));
+         }
+      }
+      else
+      {
+         i = 0xFF;
+      }
+   }
 
-		if (ret < 0) {
-			LOG_ERR("lorawan_send failed: %d", ret);
-			return;
-		}
+   while (1)
+   {
+      // Append the counter.
+      counter++;
+      data[10] = (counter >> 24) & 0xFF; // MSB
+      data[11] = (counter >> 16) & 0xFF;
+      data[12] = (counter >>  8) & 0xFF;
+      data[13] = (counter >>  0) & 0xFF; // LSB
 
-		LOG_INF("Data sent!");
-		k_sleep(DELAY);
-	}
+      LOG_INF("Sending confirmed data. Count=%u", counter);
+
+      ret = lorawan_send(2, data, sizeof(data), LORAWAN_MSG_CONFIRMED);
+
+      /*
+       * Note: The stack may return -EAGAIN if the provided data
+       * length exceeds the maximum possible one for the region and
+       * datarate. But since we are just sending the same data here,
+       * we'll just continue.
+       */
+      if (ret == -EAGAIN)
+      {
+         LOG_ERR("lorawan_send failed: %d. Continuing...", ret);
+         k_sleep(DELAY);
+         continue;
+      }
+
+      if (ret < 0)
+      {
+         LOG_ERR("lorawan_send failed: %d", ret);
+         return;
+      }
+
+      LOG_INF("Data sent!");
+      k_sleep(DELAY);
+   }
 }
